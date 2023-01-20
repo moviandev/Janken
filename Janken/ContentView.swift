4a48eb76d02bfec9439da8b6218f4759dde588e8
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-20 00:40:29 -0300

    Finish UI and add logic to the game

--- Janken/ContentView.swift
+++ Janken/ContentView.swift
@@ -33,11 +33,11 @@ struct Background: View {
         !changeBackground ?
         RadialGradient(stops: [
             .init(color: Color(red: 1, green: 0.879, blue: 0.360), location: 0.3),
-            .init(color: Color(red: 0, green: 0.708, blue: 0), location: 0.3)], center: .top, startRadius: 60, endRadius: 350)
+            .init(color: Color(red: 0, green: 0.708, blue: 0), location: 0.3)], center: .top, startRadius: 30, endRadius: 300)
         .ignoresSafeArea() :
         RadialGradient(stops: [
             .init(color: Color(red: 0, green: 0.708, blue: 0), location: 0.3),
-            .init(color: Color(red: 1, green: 0.879, blue: 0.360), location: 0.3)], center: .top, startRadius: 60, endRadius: 350)
+            .init(color: Color(red: 1, green: 0.879, blue: 0.360), location: 0.3)], center: .top, startRadius: 30, endRadius: 300)
         .ignoresSafeArea()
         
     }
@@ -46,40 +46,103 @@ struct Background: View {
 struct ContentView: View {
     @State private var moves = ["🪨", "📄", "✂️"]
         .shuffled()
-
+    
     @State private var shouldWin = Bool.random()
     @State private var appMove = Int.random(in: 0..<3)
     @State private var score = 0
+    @State private var rounds = 0
+    @State private var showingGameOver = false
     
     var body: some View {
         ZStack {
             Background(changeBackground: shouldWin)
             
-            VStack(spacing: 30) {
-                Title(text: "Janken Game")
-                
-                HStack(spacing: 50) {
-                    ForEach(0..<3, id: \.self) {number in
-                        Button {
-                            tappedEmoji(number)
-                        } label: {
-                            EmojiButton(emoji: moves[number])
+            VStack {
+                Spacer()
+                Spacer()
+                VStack {
+                    Title(text: "Janken Game")
+                    VStack {
+                        Text("App's choice: \(moves[appMove])")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        HStack(spacing: 50) {
+                            ForEach(0..<3, id: \.self) {number in
+                                Button {
+                                    tappedEmoji(number)
+                                } label: {
+                                    EmojiButton(emoji: moves[number])
+                                }
+                                .alert("Game over!", isPresented: $showingGameOver) {
+                                    Button("Play again", role: .cancel, action: playAgain)
+                                } message: {
+                                    Text("Do you wanna play again?")
+                                }
+                            }
                         }
                     }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 20)
+                    .background(.ultraThinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
                 }
+                Spacer()
+                Spacer()
+                Spacer()
+                Text("Score: \(score)")
+                    .font(.title.bold())
+                    .foregroundStyle(.secondary)
+                Spacer()
             }
             .padding()
         }
     }
     
     func tappedEmoji(_ userChoice: Int) {
-        if shouldWin && appMove == userChoice {
+        if shouldWin && isWinner(moves[appMove], moves[userChoice]) && moves[appMove] != moves[userChoice] {
             score +=  1
+        } else if moves[appMove] == moves[userChoice] {
+            score = score
         } else {
-            score -= 1
+            if !shouldWin && !isWinner(moves[appMove], moves[userChoice]) {
+             score += 1
+            } else {
+                score -= score > 0 ? 1 : 0
+            }
         }
         
+        rounds += 1
+        
+        if rounds == 6 {
+            showingGameOver = true
+            return
+        }
+        
+        chooseAgain()
+    }
+    
+    func chooseAgain() {
+        appMove = Int.random(in: 0..<3)
         shouldWin.toggle()
+    }
+    
+    func playAgain() {
+        score = 0
+        chooseAgain()
+    }
+    
+    func isWinner(_ appChoice: String, _ choosed: String) -> Bool {
+        switch appChoice {
+        case "✂️":
+            return choosed == "🪨"
+        case "📄":
+            return choosed == "✂️"
+        case "🪨":
+            return choosed == "📄"
+        default:
+            return false
+        }
     }
 }
 
